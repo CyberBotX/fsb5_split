243584b9af71c730a1f3f2a9b674ea95bb9c3f99
Naram Qashat <cyberbotx@cyberbotx.com>
2021-03-31 23:24:43 -0400

    Update to .NET 5: * Use .NET SDK-style csproj. * Remove AssemblyInfo.cs, instead putting that information into the csproj. * Add launchSettings.json to .gitignore (Visual Studio 2019 uses this file now instead of a .csproj.user file). * Add build configurations for building with FMOD SoundSystem API. * Force x86 due to the FMOD SoundSystem DLL. * Add MIT license. * Update README.

--- Program.cs
+++ Program.cs
@@ -3,41 +3,37 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
+#nullable enable
+
 namespace fsb5_split
 {
 	static class Program
 	{
 		class FSB5Header
 		{
-			public byte[] id { get; set; }
+			public byte[] id { get; set; } = null!;
 			public int version { get; set; }
 			public int numSamples { get; set; }
 			public int shdrSize { get; set; }
 			public int nameSize { get; set; }
 			public int dataSize { get; set; }
 			public uint mode { get; set; }
-			public byte[] extra { get; set; }
-			public byte[] zero { get; set; }
-			public byte[] hash { get; set; }
-			public byte[] dummy { get; set; }
+			public byte[]? extra { get; set; }
+			public byte[] zero { get; set; } = null!;
+			public byte[] hash { get; set; } = null!;
+			public byte[] dummy { get; set; } = null!;
 		}
 
-		static Func<BinaryReader, uint> fr32;
+		static Func<BinaryReader, uint> fr32 = null!;
 
-		static uint fri32(BinaryReader br)
-		{
-			return br.ReadUInt32();
-		}
+		static uint fri32(BinaryReader br) => br.ReadUInt32();
 
-		static uint frb32(BinaryReader br)
-		{
-			return BitConverter.ToUInt32(br.ReadBytes(4).Reverse().ToArray(), 0);
-		}
+		static uint frb32(BinaryReader br) => BitConverter.ToUInt32(br.ReadBytes(4).Reverse().ToArray(), 0);
 
 		static int CheckSignEndian(BinaryReader br)
 		{
 			br.BaseStream.Position = 0;
-			var sign = br.ReadBytes(4);
+			byte[] sign = br.ReadBytes(4);
 			br.BaseStream.Position = 0;
 
 			if (sign[0] == 'F' && sign[1] == 'S' && sign[2] == 'B')
@@ -65,10 +61,7 @@ namespace fsb5_split
 			fsb5Header.nameSize = (int)fr32(br);
 			fsb5Header.dataSize = (int)fr32(br);
 			fsb5Header.mode = fr32(br);
-			if (fsb5Header.version == 0)
-				fsb5Header.extra = br.ReadBytes(4);
-			else
-				fsb5Header.extra = null;
+			fsb5Header.extra = fsb5Header.version == 0 ? br.ReadBytes(4) : null;
 			fsb5Header.zero = br.ReadBytes(8);
 			fsb5Header.hash = br.ReadBytes(16);
 			fsb5Header.dummy = br.ReadBytes(8);
@@ -76,41 +69,7 @@ namespace fsb5_split
 			return (int)(br.BaseStream.Position - oldPosition);
 		}
 
-		static Func<uint, uint> GET_FSB5_OFFSET = X => (X >> 7) * 0x20;
-
-		static void InternalCopyStream(this Stream input, Stream output, int bufferSize, int bytesToWrite)
-		{
-			if (output == null)
-				throw new ArgumentException("Output Stream was null", "output");
-			if (!input.CanRead && !input.CanWrite)
-				throw new ObjectDisposedException("input", "Stream closed");
-			if (!output.CanRead && !output.CanWrite)
-				throw new ObjectDisposedException("output", "Stream closed");
-			if (!input.CanRead)
-				throw new NotSupportedException("Unreadable input Stream");
-			if (!output.CanWrite)
-				throw new NotSupportedException("Unwriteable output Stream");
-			var buffer = new byte[bufferSize];
-			int totalWritten = 0;
-			while (true)
-			{
-				int toRead = Math.Min(bufferSize, bytesToWrite - totalWritten);
-				if (toRead <= 0)
-					break;
-				int read = input.Read(buffer, 0, toRead);
-				if (read <= 0)
-					break;
-				output.Write(buffer, 0, read);
-				totalWritten += read;
-			}
-		}
-
-		public static void CopyStream(this Stream input, Stream output, int bytesToWrite)
-		{
-			if (input == null)
-				throw new ArgumentException("Input Stream was null", "input");
-			input.InternalCopyStream(output, 81920, bytesToWrite);
-		}
+		static readonly Func<uint, uint> GET_FSB5_OFFSET = X => (X >> 7) * 0x20;
 
 		static void Main(string[] args)
 		{
@@ -134,156 +93,148 @@ namespace fsb5_split
 			if (!Directory.Exists(outputDirectory))
 				Directory.CreateDirectory(outputDirectory);
 
-			using (var br = new BinaryReader(File.OpenRead(args[0])))
+			using var br = new BinaryReader(File.OpenRead(args[0]));
+			int sign = CheckSignEndian(br);
+			if (sign < 0)
 			{
-				int sign = CheckSignEndian(br);
-				if (sign < 0)
+				Console.WriteLine("Invalid file detected.");
+				return;
+			}
+			if (sign != '5')
+			{
+				Console.WriteLine("This tool is designed for FSB5 files only.");
+				return;
+			}
+
+			var fsb5Header = new FSB5Header();
+			int fhSize = ReadFSB5Header(br, fsb5Header);
+			uint nameOffset = (uint)(fhSize + fsb5Header.shdrSize);
+			uint fileOffset = (uint)(nameOffset + fsb5Header.nameSize);
+			uint baseOffset = fileOffset;
+
+			bool pcmEndian = (fsb5Header.zero[4] & 1) == 1;
+
+			for (int i = 0; i < fsb5Header.numSamples; ++i)
+			{
+				uint offset = fr32(br);
+
+				uint type = offset & 0x7F;
+				byte[] shdrData = BitConverter.GetBytes(type);
+				shdrData = shdrData.Concat(br.ReadBytes(4)).ToArray();
+				offset = GET_FSB5_OFFSET(offset); // This is the offset into the file section
+
+				long currOffset;
+				while ((type & 1) == 1)
 				{
-					Console.WriteLine("Invalid file detected.");
-					return;
-				}
-				if (sign != '5')
-				{
-					Console.WriteLine("This tool is designed for FSB5 files only.");
-					return;
-				}
-
-				var fsb5Header = new FSB5Header();
-				int fhSize = ReadFSB5Header(br, fsb5Header);
-				uint nameOffset = (uint)(fhSize + fsb5Header.shdrSize);
-				uint fileOffset = (uint)(nameOffset + fsb5Header.nameSize);
-				uint baseOffset = fileOffset;
-
-				bool pcmEndian = (fsb5Header.zero[4] & 1) == 1;
-
-				for (int i = 0; i < fsb5Header.numSamples; ++i)
-				{
-					uint offset = fr32(br);
-
-					uint type = offset & 0x7F;
-					var shdrData = BitConverter.GetBytes(type);
-					shdrData = shdrData.Concat(br.ReadBytes(4)).ToArray();
-					offset = GET_FSB5_OFFSET(offset); // This is the offset into the file section
-
-					long currOffset;
-					while ((type & 1) == 1)
-					{
-						uint t32 = fr32(br);
-						shdrData = shdrData.Concat(BitConverter.GetBytes(t32)).ToArray();
-						type = t32 & 1;
-						int len = (int)((t32 & 0xFFFFFF) >> 1);
-						t32 >>= 24;
-						currOffset = br.BaseStream.Position;
-						shdrData = shdrData.Concat(br.ReadBytes(len)).ToArray();
-						currOffset += len;
-						br.BaseStream.Position = currOffset;
-					}
-
+					uint t32 = fr32(br);
+					shdrData = shdrData.Concat(BitConverter.GetBytes(t32)).ToArray();
+					type = t32 & 1;
+					int len = (int)((t32 & 0xFFFFFF) >> 1);
+					t32 >>= 24;
 					currOffset = br.BaseStream.Position;
-					uint size;
-					if (br.BaseStream.Position < nameOffset)
-					{
-						size = fr32(br);
-						if (size == 0)
-							size = (uint)br.BaseStream.Length;
-						else
-							size = GET_FSB5_OFFSET(size) + baseOffset;
-					}
-					else
-						size = (uint)br.BaseStream.Length;
+					shdrData = shdrData.Concat(br.ReadBytes(len)).ToArray();
+					currOffset += len;
 					br.BaseStream.Position = currOffset;
-					fileOffset = baseOffset + offset;
-					size -= fileOffset;
+				}
 
-					string name = "";
-					if (fsb5Header.nameSize != 0)
-					{
-						currOffset = br.BaseStream.Position;
-						br.BaseStream.Position = nameOffset + i * 4;
-						br.BaseStream.Position = nameOffset + fr32(br);
-						do
-						{
-							byte c = br.ReadByte();
-							if (c == 0)
-								break;
-							name += (char)c;
-						} while (true);
-						br.BaseStream.Position = currOffset;
-					}
+				currOffset = br.BaseStream.Position;
+				uint size;
+				if (br.BaseStream.Position < nameOffset)
+				{
+					size = fr32(br);
+					size = size == 0 ? (uint)br.BaseStream.Length : GET_FSB5_OFFSET(size) + baseOffset;
+				}
+				else
+					size = (uint)br.BaseStream.Length;
+				br.BaseStream.Position = currOffset;
+				fileOffset = baseOffset + offset;
+				size -= fileOffset;
 
-					Console.Write("Processing {0}...", name);
-
-					string outputFilename = Path.Combine(outputDirectory, name + ".fsb");
-
+				string name = "";
+				if (fsb5Header.nameSize != 0)
+				{
 					currOffset = br.BaseStream.Position;
-					br.BaseStream.Position = fileOffset;
-					// Get file
-					using (var bw = new BinaryWriter(File.Create(outputFilename)))
-					{
-						bw.Write(Encoding.ASCII.GetBytes("FSB5"));
-						bw.Write(fsb5Header.version);
-						bw.Write(1);
-						bw.Write(shdrData.Length);
-						int fullNameSize = (int)Math.Ceiling((name.Length + 5) / 16.0) * 16;
-						bw.Write(fullNameSize);
-						bw.Write(size);
-						bw.Write(fsb5Header.mode);
-						if (fsb5Header.version == 0)
-							bw.Write(fsb5Header.extra);
-						bw.Write(fsb5Header.zero);
-						bw.Write(fsb5Header.hash);
-						bw.Write(fsb5Header.dummy);
-						bw.Write(shdrData);
-						bw.Write(4);
-						bw.Write(Encoding.ASCII.GetBytes(name));
-						bw.Write((byte)0);
-						for (int j = name.Length + 5; j < fullNameSize; ++j)
-							bw.Write((byte)0);
-						br.BaseStream.CopyStream(bw.BaseStream, (int)size);
-					}
-					fileOffset += size;
-					br.BaseStream.Position = currOffset;
-
-#if FMOD
-					// Also use the FMOD API to output to a WAV file
-					FMOD.System system;
-					var result = FMOD.Factory.System_Create(out system);
-
-					string outputWAV = Path.Combine(outputDirectory, name + ".wav");
-
-					result = system.setOutput(FMOD.OUTPUTTYPE.WAVWRITER);
-
-					result = system.init(32, FMOD.INITFLAGS.NORMAL, System.Runtime.InteropServices.Marshal.StringToHGlobalAnsi(outputWAV));
-
-					FMOD.Sound sound;
-					result = system.createSound(args[0], FMOD.MODE._2D | FMOD.MODE.LOOP_OFF | FMOD.MODE.CREATESTREAM, out sound);
-
-					FMOD.Sound subsound;
-					result = sound.getSubSound(i, out subsound);
-
-					FMOD.Channel channel;
-					result = system.playSound(subsound, null, false, out channel);
-
+					br.BaseStream.Position = nameOffset + i * 4;
+					br.BaseStream.Position = nameOffset + fr32(br);
 					do
 					{
-						result = system.update();
-						if (channel != null)
-						{
-							bool playing;
-							result = channel.isPlaying(out playing);
-							if (!playing)
-								break;
-						}
+						byte c = br.ReadByte();
+						if (c == 0)
+							break;
+						name += (char)c;
 					} while (true);
+					br.BaseStream.Position = currOffset;
+				}
 
-					result = sound.release();
-					result = system.close();
-					result = system.release();
+				Console.Write($"Processing {name}...");
+
+				string outputFilename = Path.Combine(outputDirectory, $"{name}.fsb");
+
+				currOffset = br.BaseStream.Position;
+				br.BaseStream.Position = fileOffset;
+				// Get file
+				using (var bw = new BinaryWriter(File.Create(outputFilename)))
+				{
+					bw.Write(Encoding.ASCII.GetBytes("FSB5"));
+					bw.Write(fsb5Header.version);
+					bw.Write(1);
+					bw.Write(shdrData.Length);
+					int fullNameSize = (int)Math.Ceiling((name.Length + 5) / 16.0) * 16;
+					bw.Write(fullNameSize);
+					bw.Write(size);
+					bw.Write(fsb5Header.mode);
+					if (fsb5Header.version == 0)
+						bw.Write(fsb5Header.extra!);
+					bw.Write(fsb5Header.zero);
+					bw.Write(fsb5Header.hash);
+					bw.Write(fsb5Header.dummy);
+					bw.Write(shdrData);
+					bw.Write(4);
+					bw.Write(Encoding.ASCII.GetBytes(name));
+					bw.Write((byte)0);
+					for (int j = name.Length + 5; j < fullNameSize; ++j)
+						bw.Write((byte)0);
+					br.BaseStream.CopyTo(bw.BaseStream, (int)size);
+				}
+				fileOffset += size;
+				br.BaseStream.Position = currOffset;
+
+#if FMOD
+				// Also use the FMOD API to output to a WAV file
+				var result = FMOD.Factory.System_Create(out var system);
+
+				string outputWAV = Path.Combine(outputDirectory, $"{name}.wav");
+
+				result = system.setOutput(FMOD.OUTPUTTYPE.WAVWRITER);
+
+				result = system.init(32, FMOD.INITFLAGS.NORMAL, System.Runtime.InteropServices.Marshal.StringToHGlobalAnsi(outputWAV));
+
+				result = system.createSound(args[0], FMOD.MODE._2D | FMOD.MODE.LOOP_OFF | FMOD.MODE.CREATESTREAM, out var sound);
+
+				result = sound.getSubSound(i, out var subsound);
+
+				result = system.playSound(subsound, null!, false, out var channel);
+
+				do
+				{
+					result = system.update();
+					if (channel is not null)
+					{
+						result = channel.isPlaying(out bool playing);
+						if (!playing)
+							break;
+					}
+				} while (true);
+
+				result = sound.release();
+				result = system.close();
+				result = system.release();
 #endif
 
-					Console.WriteLine(" DONE!");
-				}
+				Console.WriteLine(" DONE!");
 			}
 		}
 	}
 }
+
+#nullable restore
--- README.md
+++ README.md
@@ -1,16 +1,21 @@
+Primary repository on [GitHub](https://github.com/CyberBotX/fsb5_split).
+
 # fsb5_split
 
-This is a tool to split a multi-stream FSB5 into multiple single-stream FSB5s. The tool was inspired in part by a similar tool by hcs that was designed for FSB3 and FSB4.
+This is a tool to split a multi-stream FSB5 into multiple single-stream FSB5s. The tool was inspired in part by a
+similar tool by hcs that was designed for FSB3 and FSB4.
 
 If you have an FSB3 or FSB4 that needs splitting, you can get *fsbii* from [hcs64.com](http://hcs64.com/vgm_ripping.html).
 
 ## Compiling
 
-You will need to compile the tool with Visual Studio, at least version 2010 (the earliest with .NET 4 support as well as the earlier that supports the code's Solution/Project files).
+You will need to compile the tool with Visual Studio, at least version 2019 (the earliest with .NET 5 support) or on
+the command line via .NET 5's `dotnet` command.
 
-Additionally, you can include support for outputting an unlooped .wav file for each new FSB5 through use of the [FMOD Studio API](http://www.fmod.org/download/#StudioAPI) by including *FMOD* in your *Conditional compiler symbols* under the Build settings of the project's properties.
-
-**NOTE**: Due to limitations in the MSBuild system, I could not include a way to set the directory in the same way. If you decide to use the FMOD support, you must manually change the 2 Compile paths and the PostBuildEvent in the .csproj file to point to the proper location where you installed the FMDO Studio API.
+Additionally, you can include support for outputting an unlooped .wav file for each new FSB5 through use of the
+[FMOD Studio API](http://www.fmod.org/download/#StudioAPI) by including `FMOD` in your *Conditional compiler symbols*
+under the Build settings of the project's properties. (Alternatively, just use the *Debug with FMOD* or
+*Release with FMOD* build configurations.)
 
 ## Running
 
@@ -24,4 +29,37 @@ If the output directory is not given, the new FSB files will be written to the s
 
 ## Contact
 
-If you have any questions, comments, or concerns, you may email me at cyberbotx@cyberbotx.com. Additionally, if you spot any issues, please file an issue on GitHub.
+If you have any questions, comments or concerns about fsb5_split:
+* Contact me by email: cyberbotx@cyberbotx.com (please include fsb5_split in the subject line)
+* Contact me on IRC: On the server jenna.cyberbotx.com (I will usually be under CyberBotX)
+* Contact me on Discord: CyberBotX#8477
+* Submit an issue via [GitHub's issue tracker](https://github.com/CyberBotX/fsb5_split/issues)
+
+
+## License
+
+fsb5_split is licensed as follows:
+
+```
+The MIT License (MIT)
+
+Copyright (c) 2015-2021 Naram Qashat
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
